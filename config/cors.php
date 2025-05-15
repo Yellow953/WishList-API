9bfb3a17b70685ebdbdc9864cf332d9293865508
Yellow <joemazloum953@gmail.com>
2025-05-15 12:32:51 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:4200', 'http://127.0.0.1:4200', 'http://wishlist-tutorial.s3-website.me-central-1.amazonaws.com', 'https://wishlist-tutorial.s3-website.me-central-1.amazonaws.com'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
