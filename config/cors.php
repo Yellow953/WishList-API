6568d1b6cc259a4fb0561e30276d21d5dac441e3
Yellow <joemazloum953@gmail.com>
2025-05-19 12:52:10 +0300

    new angular url

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://wishlist-tutorial.s3-website.me-central-1.amazonaws.com'],
+    'allowed_origins' => ['http://40.172.161.0'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
